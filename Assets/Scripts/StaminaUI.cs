40d67aee57c32586e9d2cba1df36148c37a0e34c
LucasaFontaine <78568019+LucasaFontaine@users.noreply.github.com>
2026-09-16 16:45:41 +1200

    Canvas follow script instead of on healthbarui script

--- Assets/Scripts/StaminaUI.cs
+++ Assets/Scripts/StaminaUI.cs
@@ -1,10 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class StaminaUI : MonoBehaviour
 {
     [SerializeField] private PlayerController2D player;
     [SerializeField] private Image[] staminaSegments;
+    [SerializeField] private float fadeSpeed = 3f;
+
+    private CanvasGroup canvasGroup;
+    private float previousStamina;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+
+        if (player != null)
+        {
+            previousStamina = player.CurrentStamina;
+            canvasGroup.alpha = previousStamina >= player.MaxStamina ? 0f : 1f;
+        }
+    }
 
     private void Update()
     {
@@ -20,5 +36,23 @@ public class StaminaUI : MonoBehaviour
 
             staminaSegments[i].fillAmount = Mathf.Clamp01(stamina - i);
         }
+
+        bool staminaUsed = stamina < previousStamina;
+        bool isFull = stamina >= player.MaxStamina;
+
+        if (staminaUsed)
+        {
+            canvasGroup.alpha = 1f;
+        }
+        else if (isFull)
+        {
+            canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, 0f, fadeSpeed * Time.deltaTime);
+        }
+        else
+        {
+            canvasGroup.alpha = 1f;
+        }
+
+        previousStamina = stamina;
     }
 }
